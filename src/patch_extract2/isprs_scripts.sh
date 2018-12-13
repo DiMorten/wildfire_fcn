43942a51f186f6a04cbaacbd67943ce4cb0d1e9e
Jorge Andres Chamorro Martinez <cmjorgeandres@gmail.com>
2018-12-12 23:48:29 -0200

    S2. test loop on source train ok. Now on adv train

--- src/patch_extract2/isprs_scripts.sh
+++ src/patch_extract2/isprs_scripts.sh
@@ -18,11 +18,11 @@ python patch_extract_2ims.py -ds=$target -val=True -atst=True -c=3 -tm='for_loop
 
 # Train on source
 python adda.py -sds=$source -tds=$target -c=3 -ibcknd=0 -ting=0 -sval=1 -s="results/source_weights_"$source".h5" -advval=1
-python adda.py -sds=$source -tds=$target -c=3 -ibcknd=0 -ting=0 -sval=1 -advval=1 -em='basic' -tm="for_loop"
+python adda.py -sds=$source -tds=$target -c=3 -ibcknd=0 -ting=0 -sval=1 -advval=1 -em='basic' 
 
 
 # Evaluate on target
-python adda.py -t=True -c=3 -ibcknd=0 -s="results7vaihinghen/source_weights_"$source".h5" -sds $target 
+python adda.py -t=True -c=3 -ibcknd=0 -s="results7vaihinghen/source_weights_"$source".h5" -sds $target -tds $target -tm="for_loop"
 
 # Adversrial train
 python adda.py -f -c=3 -ibcknd=0 -s="results7vaihinghen/source_weights_"$source".h5" -sds $source -tds=$target -advval=1 
